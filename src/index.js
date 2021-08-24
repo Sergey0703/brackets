da9e3b7172724d4065a626e517179f31d2b9e7ed
Baliasnyi Serghii <sergey070373@gmail.com>
2021-08-24 18:56:15 +0300

    fix:array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrConfig=[];
+  let br=[];
+  
+  for(let i=0;i<bracketsConfig.length;i++){
+    br=String(bracketsConfig[i]).split(',');
+        
+    arrConfig.push(`${br[0]}${br[1]}`);
+  }
+
+
+  //console.log('str=', str, ' bracketsConfig=',bracketsConfig,' arr=',arrConfig);
+  let len=str.length;
+  for (let s=0; s<len;s++){
+  for (let j=0; j<arrConfig.length;j++){
+   
+     str=str.replace(arrConfig[j],'');
+     
+  }
+  }
+  
+//console.log('answ=',str);
+ return str.length===0;
 }
